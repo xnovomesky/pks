a66198e2d17739b203b5a181cfd30d33534e2d92
xnovomesky <xnovomesky@stuba.sk>
2025-10-29 00:25:29 +0100

    Update main.py

--- tester/main.py
+++ tester/main.py
@@ -3,22 +3,57 @@ import time
 import os
 import threading
 import json
+from crc import Calculator, Crc8
+#1 ThermoNode 
+#2 WindSense 
+#3 RainDetect
+#4 AirQualityBox
+#{"device": "ThermoNode", "type": "data", "token": "idk", "timestamp": int(time.time()), "battery_low": True, "data": "hi", "crc": ""}
+class Device:
+    def __init__(self,type):
+        self.type = type
+        self.token = ""
+        self.battery_low = False
 
 class Tester:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.ip = "127.0.0.1"
         self.port = 6666
+        self.calculator =Calculator(Crc8.CCITT)
+        self.devices = []
+        self.devices.append(Device("ThermoNode"))
+        self.devices.append(Device("WindSense"))
+        self.devices.append(Device("RainDetect"))
+        self.devices.append(Device("AirQualityBox"))
+    
+    def checksum(self, message):
+        message_copy = message.copy()
+        if "crc" in message_copy:
+            message_copy['crc'] = ""
+        data = json.dumps(message_copy, sort_keys=True).encode('utf-8')
+        return self.calculator.checksum(data)
         
     def send_message(self, msg):
+        msg['crc'] = self.checksum(msg)
         self.sock.sendto(json.dumps(msg).encode(), (self.ip, self.port))
-    
+
     def generate_messages(self):
+        print("Registering devices.")
+        for device in self.devices:
+            msg = {"type": "registration", "device": device.type, "timestamp": int(time.time()), "battery_low": False, "crc": ""}
+            self.send_message(msg)
+            data, addr = self.sock.recvfrom(1024)
+            resp = json.loads(data.decode())
+            if resp['type'] == "registration-token":
+                device.token = resp['token']
+        
         print("Started generating random messages.")
         while True:
-            message = {"device": "ThermoNode"+"-"+"tokenlol", "timestamp": int(time.time()), "low_battery": True, "data": "hi", "crc": ""}
-            self.send_message(message)
-            time.sleep(1)
+            for device in self.devices:
+                message = {"device": device.type,"type": "data", "token": device.token, "timestamp": int(time.time()), "battery_low": False, "data": "hi", "crc": ""}
+                self.send_message(message)
+                time.sleep(1)
             
     def clear_screen(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -35,7 +70,8 @@ class Tester:
             print("Tester Menu:")
             print("1. Configure IP/Port")
             print("2. Generate Messages")
-            print("3. Exit")
+            print("3. Custom Message")
+            print("4. Exit")
             choice = input("Choose: ")
             if choice == "1":
                 self.configure()
@@ -46,9 +82,6 @@ class Tester:
             elif choice == "3":
                 exit()
                 break
-
-
-
 
 tester = Tester()
 tester.menu()
