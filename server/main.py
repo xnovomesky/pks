cd53b1e918ba947d4289abc1151ebf30b4e18f71
xnovomesky <xnovomesky@stuba.sk>
2025-10-29 22:18:43 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -3,19 +3,16 @@ import time
 import os
 import threading
 import json
+import random
 from crc import Calculator, Crc8
-#1 ThermoNode 
-#2 WindSense 
-#3 RainDetect
-#4 AirQualityBox
-#{"device": "ThermoNode", "type": "data", "token": "idk", "timestamp": int(time.time()), "battery_low": True, "data": "hi", "crc": ""}
-            
+
 class Server:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.ip = "127.0.0.1"
         self.port = 6666
         self.tokens = {}
+        self.no_ack_list = {}
         self.listening = False
         self.calculator =Calculator(Crc8.CCITT)
 
@@ -26,43 +23,66 @@ class Server:
         data = json.dumps(message_copy, sort_keys=True).encode('utf-8')
         return self.calculator.checksum(data)
         
-    def send_message(self, msg, addr):
-        msg['crc'] = self.checksum(msg)
-        self.sock.sendto(json.dumps(msg).encode(), addr)
+    def send_message(self, message, addr):
+        message['crc'] = self.checksum(message)
+        self.sock.sendto(json.dumps(message).encode(), addr)
         
     def listen(self):
         self.sock.bind((self.ip, self.port))
-        print("Started listening.")
-        time.sleep(1.55)
         self.listening = True
         while self.listening:
             data, addr = self.sock.recvfrom(1024)
-            #print(addr,"message:",data,time.time())
-
             message = json.loads(data.decode())
-            #print(self.checksum(message), message['crc'])
+
             if self.checksum(message) != message['crc']:
-                print(f"WARNING: {message['device']} CORRUPTED DATA at {message['timestamp']}.")
-
+                print(f"INFO: {message['device']} CORRUTPED DATA at {message['timestamp']}. REQUESTING DATA ")
+                self.send_message({"type": "resend", "device": message['device'], "timestamp": int(time.time()),"crc": ""}, addr)
+                continue
+                
             if message['type'] == "registration":
-
                 token = message['device'] + "-" + str(message['timestamp'])
-
                 self.tokens[message['device']] = token
-
-                print(f"INFO: {message['device']} REGISTERED at {message['timestamp']}")
-                self.send_message({"type": "registration-token", "device": message['device'], "token": token, "timestamp": int(time.time()), "battery_low": False, "crc": ""}, addr)
+                print(f"INFO: {message['device']} REGISTERED at {message['timestamp']} ",addr)
+                self.send_message({"type": "token", "device": message['device'], "token": token, "timestamp": int(time.time()), "battery_low": False, "crc": ""}, addr)
 
             elif message['type'] == "data":
                 if message['device'] not in self.tokens or message['token'] != self.tokens[message['device']]:
+                    print(message)
                     print("wrong token gg")
-                    self.send_message({"type": "invalid_token", "device": message['device'], "timestamp": int(time.time()), "battery_low": False, "crc": ""}, addr)
+                    self.send_message({"type": "invalid_token", "device": message['device'], "timestamp": int(time.time()), "crc": ""}, addr)
                     continue
 
                 if  message['battery_low'] == False:
-                    print(f"INFO: {message['device']} DATA: {message['data']}")
+                    print(f"{message['timestamp']} - {message['device']} ")
+                    print("; ".join([f"{k}: {v}" for k, v in message['data'].items()]))
                 else:
-                    print(f"WARNING: LOW BATTERY {message['device']} DATA: {message['data']}")
+                    print(f"{message['timestamp']} - WARNING: LOW BATTERY {message['device']} ")
+                    print("; ".join([f"{k}: {v}" for k, v in message['data'].items()]))
+
+                if message['device'] in self.no_ack_list and self.no_ack_list[message['device']] > 0:
+                    self.no_ack_list[message['device']] -= 1
+                    #print("not sending ack")
+                else:
+                    self.send_message({"type": "ack", "device": message['device'], "ack_time": message['timestamp'], "timestamp": int(time.time()),"crc": ""}, addr)
+                    #print("sending ack")
+                    
+    def disable_ack(self):
+        if 1:
+            self.clear_screen()
+            print("Select sensor:")
+            print("1. ThermoNode")
+            print("2. WindSense")
+            print("3. RainDetect")
+            print("4. AirQualityBox")
+            choice = input("Choose: ")
+            if choice == "1":
+                self.no_ack_list["ThermoNode"] = 3
+            elif choice == "2":
+                self.no_ack_list["WindSense"] = 3
+            elif choice == "3":
+                self.no_ack_list["RainDetect"] = 3
+            elif choice == "4":
+                self.no_ack_list["AirQualityBox"] = 3
                 
     def clear_screen(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -79,19 +99,21 @@ class Server:
             print("Server Menu:")
             print("1. Configure IP/Port")
             print("2. Listen", ("[Running]" if self.listening else "[Stopped]"))
-            print("3. Exit")
+            print("3. Disable ACK")
+            print("4. Exit")
             choice = input("Choose: ")
             if choice == "1":
                 self.configure()
                 time.sleep(1.5)
             elif choice == "2":
                 if self.listening:
-                    print("Stopped listening.")
                     self.listening = False
                 else:
                     threading.Thread(target=self.listen).start()
-                time.sleep(1.5)
             elif choice == "3":
+                self.disable_ack()
+                time.sleep(1.5)
+            elif choice == "4":
                 exit()
                 break
 
