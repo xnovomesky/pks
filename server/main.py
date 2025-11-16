5ac31443f2d0a243cf3548fde2d0b7cab5891ec8
xnovomesky <xnovomesky@stuba.sk>
2025-11-16 17:40:14 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -14,6 +14,12 @@ class Server:
         self.no_ack_list = {}
         self.listening = False
         self.calculator =Calculator(Crc8.CCITT)
+        self.last_received = {}
+        self.device_to_addr = {}
+        self.device_status = {}
+        self.verify_attempts = {}
+        self.verify_last_sent = {}
+        self.active_verifications = set()
 
     def checksum(self, message):
         message_copy = message.copy()
@@ -25,7 +31,43 @@ class Server:
     def send_message(self, message, addr):
         message['crc'] = self.checksum(message)
         self.sock.sendto(json.dumps(message).encode(), addr)
-        
+    
+    def send_verify(self, device):
+        addr = self.device_to_addr[device]
+        message = {"type": "verify", "device": device, "timestamp": int(time.time()), "crc": ""}
+        self.send_message(message, addr)
+    def verify_process(self, device):
+        self.verify_attempts[device] = 0
+        self.send_verify(device)
+        self.verify_attempts[device] += 1
+        sent_time = time.time()
+        self.verify_last_sent[device] = sent_time
+        time.sleep(1)
+        if self.last_received.get(device, 0) > sent_time:
+            self.verify_attempts[device] = 0
+            self.active_verifications.remove(device)
+            return
+        print(f"INFO: {device} DISCONNECTED")
+        self.device_status[device] = "DISCONNECTED"
+        while self.verify_attempts.get(device, 0) < 10:
+            time_to_wait = 5 - (time.time() - self.verify_last_sent.get(device, 0))
+            if time_to_wait > 0:
+                time.sleep(time_to_wait)
+            if self.device_status.get(device) == "CONNECTED":
+                break
+            self.send_verify(device)
+            self.verify_attempts[device] += 1
+            self.verify_last_sent[device] = time.time()
+        self.active_verifications.remove(device)
+    
+    def inactivity_monitor(self):
+        while self.listening:
+            now = time.time()
+            for device in list(self.last_received.keys()):
+                if now - self.last_received.get(device, 0) > 15 and device not in self.active_verifications and self.device_status.get(device, "CONNECTED") == "CONNECTED":
+                    self.active_verifications.add(device)
+                    threading.Thread(target=self.verify_process, args=(device,)).start()
+            time.sleep(1)
     def listen(self):
         self.sock.bind((self.ip, self.port))
         self.listening = True
@@ -43,8 +85,21 @@ class Server:
                 self.tokens[message['device']] = token
                 print(f"INFO: {message['device']} REGISTERED at {message['timestamp']} ")
                 self.send_message({"type": "token", "device": message['device'], "token": token, "timestamp": int(time.time()), "crc": ""}, addr)
-
+                self.device_to_addr[message['device']] = addr
+                self.last_received[message['device']] = time.time()
+                self.device_status[message['device']] = "CONNECTED"
+                self.verify_attempts[message['device']] = 0
+                self.verify_last_sent[message['device']] = 0
+            
+            elif message['type'] == "verify_response":
+                if message['device'] not in self.tokens or message.get('token', '') != self.tokens[message['device']]:
+                    self.send_message({"type": "invalid_token", "device": message['device'], "timestamp": int(time.time()), "crc": ""}, addr)
+                    continue
+                self.last_received[message['device']] = time.time()
+                self.device_status[message['device']] = "CONNECTED"
+                self.verify_attempts[message['device']] = 0
             elif message['type'] == "data":
+            
                 if message['device'] not in self.tokens or message['token'] != self.tokens[message['device']]:
                     #print("wrong token gg")
                     self.send_message({"type": "invalid_token", "device": message['device'], "timestamp": int(time.time()), "crc": ""}, addr)
@@ -56,7 +111,10 @@ class Server:
                 else:
                     print(f"{message['timestamp']} - WARNING: LOW BATTERY {message['device']} ")
                     print(" ".join([f"{k}: {v};" for k, v in message['data'].items()]))
-
+                self.device_to_addr[message['device']] = addr
+                self.last_received[message['device']] = time.time()
+                self.device_status[message['device']] = "CONNECTED"
+                self.verify_attempts[message['device']] = 0
                 if message['device'] in self.no_ack_list and self.no_ack_list[message['device']] > 0:
                     self.no_ack_list[message['device']] -= 1
                     #print("not sending ack")
@@ -108,6 +166,7 @@ class Server:
                     self.listening = False
                 else:
                     threading.Thread(target=self.listen).start()
+                    threading.Thread(target=self.inactivity_monitor).start()
             elif choice == "3":
                 self.disable_ack()
                 time.sleep(1.5)
